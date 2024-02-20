4a6e1a415c6a6785d76b8177f8f50c2d6b212259
Varun Ram S <80676450+varunram2001@users.noreply.github.com>
2024-02-21 00:19:32 +0530

    Add files via upload

---
+++ Day003/RunningSum1DArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int total_sum=0;
+        for(int i=1;i<nums.length;i++)
+        {
+            nums[i]=nums[i]+nums[i-1];
+        }
+        return nums;
+    
+        
+        
+    }
+}
