7a5f00084177734019c45a70733a9c12b0248fd2
Varun Ram S <80676450+varunram2001@users.noreply.github.com>
2024-02-23 00:18:00 +0530

    Add files via upload

---
+++ Day003/ThirdMaximumNumber.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        long first=Long.MIN_VALUE;
+        long second=Long.MIN_VALUE;
+        long third=Long.MIN_VALUE;
+
+        for(int num:nums)
+        {
+            if(num>first)
+            {
+                third=second;
+                second=first;
+                first=num;
+            }
+            else if(first > num && num > second)
+            {
+                third=second;
+                second=num;
+            }
+            else if(second>num && num>third)
+            {
+                third=num;
+            }
+        }
+        return third==Long.MIN_VALUE ? (int)first:(int)third;
+        
+        
+    }
+}
