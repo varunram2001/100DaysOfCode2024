fac4e32cb3c986834330d424cad408408f07c322
Varun Ram S <80676450+varunram2001@users.noreply.github.com>
2024-03-04 22:39:31 +0530

    Add files via upload

---
+++ Day006/backspace string compare - optmized.java
@@ -0,0 +1,27 @@
+ class Solution {
+
+  boolean backspaceCompare(String s, String t) {
+    return getActual(s).equals(getActual(t));
+  }
+
+  private String getActual(String input) {
+
+    StringBuilder actualString = new StringBuilder();
+    int hashCount = 0;
+
+    for (int i = input.length() - 1; i >= 0 ; i--) {
+      if (input.charAt(i) == '#') {
+        hashCount++;
+        continue;
+      }
+      if (hashCount > 0) {
+        hashCount--;
+      } else {
+        // If no backspace, just insert at beginning
+        actualString.insert(0, input.charAt(i));
+      }
+    }
+
+    return actualString.toString();
+  }
+}
